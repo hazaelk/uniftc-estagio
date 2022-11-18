0cdeb83c5b77b11c41b9264a025d782afd91345f
nodgear <matheus.silva3@ftc.edu.br>
2022-11-18 00:20:02 -0300

    feat: relative imports to jest TS files

--- jest.config.js
+++ jest.config.js
@@ -4,13 +4,15 @@ module.exports = {
   moduleNameMapper: {
     '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
     '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$': `<rootDir>/__mocks__/fileMock.js`,
-    '^@components(.*)$': '<rootDir>/src/components$1',
+    '^@components(.*)$': '<rootDir>/components$1',
+    '^@hooks(.*)$': '<rootDir>/hooks$1',
+    '^@lib(.*)$': '<rootDir>/lib$1',
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.tsx'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
   transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
-  moduleDirectories: ['node_modules', './']
+  moduleDirectories: ['node_modules', '<rootDir>']
 };
