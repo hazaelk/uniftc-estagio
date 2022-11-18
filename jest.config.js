cff150d032be5a3323fdd19c16d8ddeff9dfd562
nodgear <matheus.silva3@ftc.edu.br>
2022-11-17 23:57:34 -0300

    chore: workaround to use SWC instead babel on jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/*.d.ts', '!**/node_modules/**'],
+  moduleNameMapper: {
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$': `<rootDir>/__mocks__/fileMock.js`,
+    '^@components(.*)$': '<rootDir>/src/components$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.tsx'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
+  moduleDirectories: ['node_modules', './']
+};
