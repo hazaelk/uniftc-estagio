f414ac2eb0e61f03d3a7164b61e1b2ed1fe5b638
Nodge <wbteuz@gmail.com>
2022-11-28 01:32:35 -0300

    fix: proper routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,60 +1,62 @@
-import Image from 'next/image'
-import Imagetest from '../../assets_test/testing-img-home.png'
-import Ftclogo from '../../assets_test/ftc-logo.svg'
-import styles from '../styles/Home.module.css'
+import CursoCard from "@/components/layouts/CursoCard";
+import HeaderLayout from "@/components/layouts/HeaderLayout";
+import Image from "next/image";
+import foto1 from '../public/cards_publicidade/foto1.png'
+import foto2 from '../public/cards_publicidade/foto2.png'
+import foto3 from '../public/cards_publicidade/foto3.png'
 
-interface Props {
-  children: React.ReactNode;
-}
 
-const HomeLayout = ({ children }: Props) => {
-  return <div className='flex items-center justify-center w-full h-screen text-black text-sm font-light bg-blue-50'>
-    {children}
+export default function LandingPage() {
+const listaCursos = [
+    {title: 'Banco de Dados', description: 'Curso de BD completo'},
+    {title: 'Programação', 
+    description: 'Curso de programação completo'},
+    {title: 'Infraestrutura', 
+    description: 'Curso de Infraestrutura completo'},
+    {title: 'Redes', 
+    description: 'Curso de Redes completo'},
+    {title: 'Manutenção de computadores', 
+    description: 'Curso de Manutenção de computadores completo'},
+    {title: 'Boas práticas de programação', 
+    description: 'Curso de Boas práticas de programação completo'},
+]
+
+    return (
+        <div className="bg-blue-600 h-screen py-4 transition-all">
+            <HeaderLayout 
+            username="willian"/>
+            <section className="flex flex-col items-center mt-16">
+                <h1 className="text-white font-bold text-4xl">Lorem ipsum</h1>
+                <h5 className="text-gray-300 mt-1 mb-10 text-sm">Dolor sit amet, it ame lorem lor.</h5>
+            </section>
+            <div className="flex justify-evenly sm:relative sm:z-1 sm:left-auto sm:right-auto">
+                <Image src={foto1} 
+                className="w-1/4  m-1 sm:w-52 sm:h-56 sm:absolute sm:left-40"
+                alt={"Imagem de exemplo"}
+                />
+                <Image src={foto2}
+                className="w-1/4  m-1 sm:w-52 sm:h-56 sm:absolute sm:origin-center"
+                alt={"Imagem de exemplo"}
+                />
+                <Image src={foto3}
+                className="w-1/4 m-1 sm:w-52 sm:h-56 sm:absolute sm:right-40"
+                alt={"Imagem de exemplo"}
+                />
+            </div>
+            <main className="h-full w-full mt-24 bg-white">
+                <div className="flex flex-col items-center justify-center">
+                    <h2 className="text-black font-medium text-2xl py-4 sm:pt-36">Recentemente adicionados:</h2>
+                    <div className="grid grid-cols-2 gap-5 sm:grid sm:grid-cols-3 sm:gap-8 sm:pt-4">
+                        {listaCursos.map((item) => {
+                            return <CursoCard key={item.title} title={item.title} description={item.description}/>
+                        })}
+                    </div>
+
+                </div>
+
+            </main>
+            
+        </div>
+    )
     
-    <div className="flex items-center flex-col justify-center w-full">
-
-      <Image
-        src={Ftclogo}
-        quality={100}
-        alt="Logo da FTC"
-        className={styles.logoStyle}
-      />
-
-      <form
-        className='w-48 relative'
-        action=""
-        method="post">
-        <label
-          className='mb-6 font-normal'
-          htmlFor="username">Usuário:</label><br></br>
-        <input
-          type="email" name="username" id="username"
-          className={styles.inputStyle} /><br></br>
-
-        <label
-          className='mb-6 font-normal'
-          htmlFor="password">Senha:</label><br></br>
-        <input
-          type="password" name="password" id="password"
-          className={styles.inputStyle} />
-
-        <button
-          className="bg-blue-600 text-white font-semibold w-full h-8 rounded-md mt-4 active:bg-blue-800"
-          type="submit">
-            <a href='/landingpage'>Conectar</a>
-          </button>
-      </form>
-
-    </div>
-    <div className="items-center w-full h-full hidden sm:flex">
-      <Image
-        className="w-full h-full"
-        src={Imagetest}
-        alt="Mulher sorrindo, olhando para trás, segurando uma mão."
-        quality={100}
-        objectFit="fill" />
-    </div>
-  </div>
 }
-
-export default HomeLayout
