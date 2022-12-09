3b782201c94828888c1960c979188c119959599d
willian <willian@emasa.local>
2022-12-09 10:59:51 -0300

    chore: css adjusts

--- components/CourseCardAvailable.tsx
+++ components/CourseCardAvailable.tsx
@@ -65,15 +65,15 @@ function CourseCardAvailable({ title, tags, cover, author, description, id }: Pr
         </button>
       </Link>
       {isHover && isAdmPage ? (
-        <div className="absolute top-1/2 -rigth-1/5 z-9 shadow-black shadow-2xl drop-shadow-2xl bg-transparent">
-          <button className="transition-all rounded-md mb-2 text-sm lg:text-lg flex items-center bg-yellow-700 text-black font-medium px-4 py-2 hover:bg-yellow-600">
+        <div className="absolute left-1/2 -ml-1/2 z-9 bg-transparent">
+          <button className="transition-all rounded-md mb-2 text-sm lg:text-lg shadow-black shadow-2xl drop-shadow-2xl flex items-center bg-yellow-700 text-black font-medium px-4 py-2 hover:bg-yellow-600">
             Editar informações
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
             </svg>
 
           </button>
-          <button className="transition-all rounded-md mb-2 text-sm lg:text-lg flex items-center bg-green-700 text-white font-medium px-4 py-2 hover:bg-green-600">
+          <button className="transition-all rounded-md mb-2 text-sm lg:text-lg shadow-black shadow-2xl drop-shadow-2xl flex items-center bg-green-700 text-white font-medium px-4 py-2 hover:bg-green-600">
             Incluir videos aulas
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
