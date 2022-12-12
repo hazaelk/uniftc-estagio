86280b6711b1c88e44137fd9b57ecbdecdda1d96
nodgear <matheus.silva3@ftc.edu.br>
2022-12-12 14:29:30 -0300

    fix: show categories if possible on mycourses

--- components/CourseCardMy.tsx
+++ components/CourseCardMy.tsx
@@ -2,11 +2,22 @@ type Props = {
   title: string
   image: string
   description: string
+  categories?: {
+    name: string
+    id: number
+  }[]
 };
 
-function CourseCardMy({title, image, description}: Props) {
+function CourseCardMy({title, image, description, categories}: Props) {
   return (
     <div className="bg-white shadow-md rounded-lg p-4 text-[#121212] cursor-pointer">
+       { categories && categories.length > 0 && <div className="mb-4 flex text-[10px] items-center gap-2">
+          {categories.map((category) => {
+            return <p className="px-2 py-1 bg-blue-100 border border-blue-200 rounded" key={`cat.${category.id}`}>
+              {category.name}
+            </p>
+          })}
+        </div> }
       <img
         alt=""
         className="object-cover w-full h-32 mb-4 rounded-md"
